5469a36462be4db882d8a48e2ff30f40ca472995
Fabricio Quintas <fabri97n@gmail.com>
2022-08-04 17:30:37 -0300

    Ended with class and his challenge

--- debugging.py
+++ debugging.py
@@ -1,15 +1,27 @@
 def divisors(num):
-    divisors = []
-    for i in range(1, num+1):
-        if num % i == 1:
-            divisors.append(i)
+    # Made it in list comprehension
+    divisors = [i for i in range(1, num+1) if num % i == 0]
+    # for i in range(1, num+1):
+    #     if num % i == 0:
+    #         divisors.append(i)
     return divisors
 
-def run():
-    num = int(input("Ingresa un número: "))
-    print(divisors(num))
-    print("Terminó mi programa")
+# Reto:
+    # Utiliza las palabras clave try, except y raise
+    # para elevar un error si el usuario ingresa un número
+    # negativo en nuestro programa de divisores
 
+def run():
+    try:
+        num = int(input("Ingresa un número: "))
+        if num <= 0:
+            raise Exception('El número debe ser mayor a 0')
+        print(divisors(num))
+        print("Terminó mi programa")
+    except ValueError:
+        print("Debes ingresar un número")
+    except Exception as ex:
+        print(ex)
 
 if __name__ == '__main__':
     run()
